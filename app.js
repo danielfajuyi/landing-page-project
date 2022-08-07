4e69b16c96f9ae91d560137123ae640cd15bb30d
danielfajuyi <ayomidedaniels121@gmail.com>
2022-08-07 05:30:31 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+// Select The Element 
+var toggle_button;
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+     toggle_button = document.querySelector(".toggle-btn");
+     big_wrapper = document.querySelector(".big-wrapper");
+     hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+    // Clone the wrapper 
+    dark = !dark;
+    console.log(dark);
+    let clone = big_wrapper.cloneNode(true);
+    if(dark) {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    }
+    else {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone); 
+
+ 
+
+    clone.addEventListener("animationend", () => {
+       
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        // Reset variable 
+        declare();
+        events();
+    });
+    
+}
+
+function events() {
+    toggle_button.addEventListener("click", toggleAnimation);
+    hamburger_menu.addEventListener("click", () => {
+        big_wrapper.classList.toggle("active");
+    });
+}
+
+events();
+
